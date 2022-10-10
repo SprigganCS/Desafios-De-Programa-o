d9830f74065af80292886e254da32dc1356d60a1
André Neves Medeiros <69917573+SprigganCS@users.noreply.github.com>
2022-10-10 18:09:03 -0300

    indentation and vertical find fix

--- CacaPalavras/main.py
+++ CacaPalavras/main.py
@@ -1,41 +1,68 @@
 def achaHorizontal(grid, palavras, lin):
     for i in range(len(palavras)):
         for j in range(lin):
-            aux = ' '.join(grid[j])
+            aux = ' '.join(grid[j]) #transforma a linha da lista em string
             index = aux.find(palavras[i])
             #print(aux, palavras[i])
             if index != -1:
-                print(palavras[i], j, index)
+                print(palavras[i], j, index) #printa a palavra, linha e coluna, normal
 
             reverse = palavras[i][::-1]
             index = aux.find(reverse)
             if index != -1:
-                print(reverse, j, index + len(palavras[i]) - 1)
+                print(reverse, j, index + len(palavras[i]) - 1) #printa a palavra, linha e coluna, reversa
+
+
+def achaVertical(grid, palavras, col, lin):
+    for i in range(len(palavras)):
+        for j in range(col):
+            aux = ''
+            for k in range(lin):
+                aux += grid[k][0][j] #transforma a coluna da lista em string
+            #print(aux) #printa a coluna em formato de linha
+            index = aux.find(palavras[i])
+            if index != -1:
+                print(palavras[i], index+1, j+1) #printa a palavra, linha e coluna, normal #precisa somar 1 pq começa em 1
+            
+            reverse = palavras[i][::-1]
+            index = aux.find(reverse)
+            if index != -1:
+                print(reverse, index + len(palavras[i])-1 +1, j+1) #printa a palavra, linha e coluna, reversa #precisa somar 1 pq começa em 1
+            
+
+        
 
 def main():
     cases = int(input()) #numero de casos
     grid=[]
     palavras=[]
+    blankline = input()
     for i in range(cases):
-        blankline = input()
         grid=[]
         palavras=[]
-        dim = input()
-        dim = dim.split()
+        dim = input() #le qtd de linhas e colunas
+        dim = dim.split() #divide em duas variaveis
         lin = int(dim[0])
         col = int(dim[1])
 
-        for j in range(col):
+        while(1): #o criterio de parada é quando a linha tiver tamanho 1 (o fim da matriz é seguido do numero de palavras), então break
             aux = input().upper()
             if len(aux) == 1:
-                #aux guarda o numero de palavras
                 break
             else:
-                grid.append([aux])
+                grid.append([aux]) #salva a linha na lista
 
-        for i in range(int(aux)):
+        for i in range(int(aux)): #aux é a quantidade de palavras a serem buscadas
             palAux = input().upper()
             palavras.append(palAux)
         
         achaHorizontal(grid, palavras, lin)
+        achaVertical(grid, palavras, col, lin)
+
+
+        try:
+            blankline = input()
+        except EOFError:
+            break
+
 main()
