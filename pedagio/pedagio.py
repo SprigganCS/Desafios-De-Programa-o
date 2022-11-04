05f793e00bddfebe5a6c7df6f96ed2dd554539bf
Arthur Rodrigues Proença <60947268+ArthurProenca@users.noreply.github.com>
2022-11-04 14:39:42 -0300

    Update pedagio.py

--- pedagio/pedagio.py
+++ pedagio/pedagio.py
@@ -1,19 +1,54 @@
-  from array import array
-from pexpect import EOF
-
 def calcula(prices, e1, e2):
     km = int(int(e2[3]) - int(e1[3]))
 
     horaE1 = e1[1].split(":")[2]
     prices = [float(i) for i in prices]
     preco = km * (prices[int(horaE1)] / 100)
+    preco+=3
     return preco
+
+def elimina_repeticoes(lista):
+    cont = 0
+    while(cont < len(lista)):
+        if(cont + 1 < len(lista) and lista[cont] == lista[cont + 1]): 
+            lista[cont][1]+=1
+           
+        
+        if(lista[cont][2][1] > 0):
+            lista[cont][1] += 2
+        cont += 1
+    return lista
+
+def calcula_diferenca_data(data1, data2):
+    data1 = data1.split(":")
+    data2 = data2.split(":")
+    data1 = [int(i) for i in data1]
+    data2 = [int(i) for i in data2]
+    
+    diferenca = [0, 0, 0]
+    diferenca[2] = data2[2] - data1[2]
+    if(diferenca[2] < 0):
+        diferenca[2] += 60
+        data2[1] -= 1
+    diferenca[1] = data2[1] - data1[1]
+    if(diferenca[1] < 0):
+        diferenca[1] += 60
+        data2[0] -= 1
+    diferenca[0] = data2[0] - data1[0]
+    return diferenca
+
+def cria_output(lista):
+    for i in lista:
+        print(f"{i[0]} ${i[1]}")
 
 numero_ocasioes = int(input())
 input()
 array_precos = []
 for i in range(numero_ocasioes):
-    prices = input()  
+    try:
+        prices = input()  
+    except EOFError:
+        break
     prices = prices.split()
     entradas = []
     while(True):
@@ -24,29 +59,23 @@ for i in range(numero_ocasioes):
         except Exception: 
             break
 
-    entradas.sort(key=lambda x: (x[0], x[1], x[-2]))
-    #print(entradas[0][1].split(":")[1]) split data
-    #print(entradas)
+  
+    entradas.sort()
+
     cont = 0
-    placas = []
+    final_array = []
     precox = 0
-    while(cont < len(entradas) / 2):
-        if(entradas[cont][0] == entradas[cont + 1][0]):
-            if(entradas[cont][0] in placas):
-                index = placas.index(entradas[cont][0])
-
-            else:
-                index = len(placas)
-                placas.append([entradas[cont][0]])    
-            
-            if(entradas[cont][2] == "enter" and entradas[cont + 1][2] == "exit"): 
-                array_precos.append(calcula(prices, entradas[cont], entradas[cont + 1]))
-            else:
-                print()
-
-        else :
-
-            cont += 1
-
+    while(cont < len(entradas)):
+        if ((cont + 1) < len(entradas)):
+            if(entradas[cont][0] == entradas[cont + 1][0]):
+                try:
+                    if(entradas[cont][2] == "enter" and entradas[cont + 1][2] == "exit"): 
+                        final_array.append([entradas[cont][0], calcula(prices, entradas[cont], entradas[cont + 1]).__round__(2), calcula_diferenca_data(entradas[cont][1], entradas[cont + 1][1])])
+                except Exception as e:
+                    pass
+                    
+                   
         cont+=1
     
+    final_array = elimina_repeticoes(final_array)
+    cria_output(final_array)
